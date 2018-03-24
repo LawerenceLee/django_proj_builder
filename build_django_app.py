bf8a3fd24111175e841f061fa6f4fbb34530fe9e
LawerenceLee <lucifitz.edward@gmail.com>
2018-03-24 12:37:24 -0700

    Removed the building of the apps folder in both

--- build_django_app.py
+++ build_django_app.py
@@ -2,66 +2,76 @@ import os
 import shutil
 
 
-def clear():
-    os.system('cls' if os.name == "nt" else 'clear')
-
-
 def build_filesys():
+    # Save paths for templates, and projects top lvl dir
     start_path = os.getcwd()
     index_path = start_path + "/PREBUILT_TEMPLATES/index.html"
     layout_path = start_path + "/PREBUILT_TEMPLATES/layout.html"
     css_path = start_path + "/PREBUILT_TEMPLATES/style.css"
+
+    # Create a django app with provided app name
     os.system("django-admin startapp {}".format(APP_NAME))
 
+    # CD into App's dir and create urls..py
     os.chdir(APP_NAME)
-    root_path = os.getcwd()
     os.system("touch urls.py")
+    # save path to App dir
+    root_path = os.getcwd()
+
+    # Add imports and url for a basic index view
     urls_starter = "from django.conf.urls import url\n\nfrom . import views"
     urls_starter += "\n\nurlpatterns = [\n"
     urls_starter += "    url(r'^$', views.index)\n]"
     os.system('echo "{}" >> urls.py'.format(urls_starter))
-    os.makedirs("templates/{}".format(APP_NAME))
 
+    # Append a basic index view function into 'views.py'
     index_view = "\ndef index(request):\n    "
     index_view += "return render(request, '{}/index.html')".format(APP_NAME)
     os.system(' echo "{}" >> views.py'.format(index_view))
 
-    os.mkdir("static")
-    os.chdir("static")
-    os.makedirs("{}/css".format(APP_NAME))
-    os.chdir("{}".format(APP_NAME))
-    os.makedirs("img".format(APP_NAME))
-    os.makedirs("js".format(APP_NAME))
-    os.chdir("../../")
-
+    # Create templates dir
+    os.makedirs("templates/{}".format(APP_NAME))
     os.chdir("templates/{}".format(APP_NAME))
+    # Move templates into App
     shutil.copy(index_path, os.getcwd()+"/")
     shutil.copy(layout_path, os.getcwd()+"/")
+    # Navigate two dirs up
+    os.chdir("../../")
 
-    os.chdir("../../static/{}/css".format(APP_NAME))
+    # Create a static dir and CD into it
+    os.makedirs("static/{}".format(APP_NAME))
+    os.chdir("static/{}".format(APP_NAME))
+    # Create js, img, and css dirs
+    os.mkdir("img")
+    os.mkdir("js")
+    os.mkdir("css")
+    # CD into css dir
+    os.chdir("css")
+    # Move style.css template to css dir
     shutil.copy(css_path, os.getcwd()+"/")
 
+    # Move entire project outside of DJANGO PROJ BUILDER dir
     shutil.move(root_path, "{}/..".format(start_path))
 
 
 def to_do_list():
     print("\nTO DO LIST:")
-    print("\t- Add 'apps.{}' to INSTALLED APPS inside your project's main settings.py\
+    print("\t- Add '{}' to INSTALLED APPS inside your project's main settings.py\
         ".format(APP_NAME))
-    print("\t- Add 'url(r'^/', include('apps.{}.urls')),' to \
+    print("\t- Add 'url(r'^/', include('{}.urls')),' to \
         \n\t  your project's main urls.py".format(APP_NAME))
     print("\t- Add the 'include' import to 'from django.conf.urls import url'")
     print("\t- Change the path of the link tag in 'layout.html'")
     print("\t- Change the title in the title tag in 'layout.html'")
     print("\t- Change the path of the extends function in 'index.html'")
-    print("\t- MODELS: Remember to use makemigrations, migrate after building your models.")
+    print("\t- MODELS: Remember to use makemigrations, migrate after building your models.\n")
 
 
 if __name__ == '__main__':
-    clear()
+    os.system('cls' if os.name == "nt" else 'clear')
     thirty_three = "-" * 33
-    print("{} DJANGO APP BUILDER {}\n".format(
+    print("{} DJANGO PROJECT BUILDER WITH APPS {}".format(
         thirty_three, thirty_three))
-    APP_NAME = raw_input("Enter an App Name: ")
+    APP_NAME = raw_input("\nEnter an App Name: ")
     build_filesys()
     to_do_list()
--- build_django_w_apps.py
+++ build_django_w_apps.py
@@ -2,60 +2,71 @@ import os
 import shutil
 
 
-def clear():
-    os.system('cls' if os.name == "nt" else 'clear')
-
-
 def build_filesys():
+    # Save paths for templates, and projects top lvl dir
     start_path = os.getcwd()
     index_path = start_path + "/PREBUILT_TEMPLATES/index.html"
     layout_path = start_path + "/PREBUILT_TEMPLATES/layout.html"
     css_path = start_path + "/PREBUILT_TEMPLATES/style.css"
+
+    # Build Django Project w/ provided project name
     os.system("django-admin startproject {}_main".format(PROJ_NAME))
+
+    # Change Project's top level directory to use '<project_name>_proj' instead
     os.rename(PROJ_NAME+"_main", PROJ_NAME+"_proj")
+    # Change into said dir
     os.chdir('{}_proj'.format(PROJ_NAME))
+
+    # Save the path just inside projects top lvl dir
     root_path = os.getcwd()
-    os.mkdir("apps")
 
-    os.chdir("apps")
-    os.system("touch __init__.py")
-    os.system("python ../manage.py startapp {}".format(APP_NAME))
+    # Create a django app with provided app name
+    os.system("python manage.py startapp {}".format(APP_NAME))
 
+    # CD into App's dir and create urls..py
     os.chdir(APP_NAME)
     os.system("touch urls.py")
+    # Add imports and url for a basic index view
     urls_starter = "from django.conf.urls import url\n\nfrom . import views"
     urls_starter += "\n\nurlpatterns = [\n"
     urls_starter += "    url(r'^$', views.index)\n]"
     os.system('echo "{}" >> urls.py'.format(urls_starter))
-    os.makedirs("templates/{}".format(APP_NAME))
 
+    # Append a basic index view function into 'views.py'
     index_view = "\ndef index(request):\n    "
     index_view += "return render(request, '{}/index.html')".format(APP_NAME)
     os.system(' echo "{}" >> views.py'.format(index_view))
 
-    os.mkdir("static")
-    os.chdir("static")
-    os.makedirs("{}/css".format(APP_NAME))
-    os.chdir("{}".format(APP_NAME))
-    os.makedirs("img".format(APP_NAME))
-    os.makedirs("js".format(APP_NAME))
-    os.chdir("../../")
-
+    # Create templates dir
+    os.makedirs("templates/{}".format(APP_NAME))
     os.chdir("templates/{}".format(APP_NAME))
+    # Move templates into App
     shutil.copy(index_path, os.getcwd()+"/")
     shutil.copy(layout_path, os.getcwd()+"/")
+    # Navigate two dirs up
+    os.chdir("../../")
 
-    os.chdir("../../static/{}/css".format(APP_NAME))
+    # Create a static dir and CD into it
+    os.makedirs("static/{}".format(APP_NAME))
+    os.chdir("static/{}".format(APP_NAME))
+    # Create js, img, and css dirs
+    os.mkdir("img")
+    os.mkdir("js")
+    os.mkdir("css")
+    # CD into css dir
+    os.chdir("css")
+    # Move style.css template to css dir
     shutil.copy(css_path, os.getcwd()+"/")
 
+    # Move entire project outside of DJANGO PROJ BUILDER dir
     shutil.move(root_path, "{}/..".format(start_path))
 
 
 def to_do_list():
     print("\nTO DO LIST:")
-    print("\t- Add 'apps.{}' to INSTALLED APPS inside {}_main/settings.py\
+    print("\t- Add '{}' to INSTALLED APPS inside {}_main/settings.py\
         ".format(APP_NAME, PROJ_NAME))
-    print("\t- Add 'url(r'^'/', include('apps.{}.urls'),' to \
+    print("\t- Add 'url(r'^/', include('{}.urls'),' to \
         \n\t  urls.py inside {}_main".format(APP_NAME, APP_NAME, PROJ_NAME))
     print("\t- Add the 'include' import to 'from django.conf.urls import url'")
     print("\t- Change the path of the link tag in 'layout.html'")
@@ -64,11 +75,11 @@ def to_do_list():
 
 
 if __name__ == '__main__':
-    clear()
+    os.system('cls' if os.name == "nt" else 'clear')
     thirty_three = "-" * 33
     print("{} DJANGO PROJECT BUILDER WITH APPS {}".format(
         thirty_three, thirty_three))
-    PROJ_NAME = raw_input("Enter a Project Name: ")
+    PROJ_NAME = raw_input("\nEnter a Project Name: ")
     APP_NAME = raw_input("Enter an App Name: ")
     build_filesys()
     to_do_list()
